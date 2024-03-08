b49eabe4a0afde29939f4011beca87fb80e7e915
Ayooon16 <michaunapierala@gmail.com>
2024-03-08 16:18:48 +0100

    Add basic js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-a
+function start() {
+    create(16);
+}
+
+function create(n) {
+    let rt = Math.floor(Math.sqrt(n));
+    let o = 0;
+
+    for (let i = 0; i < n; i++) {
+        if (i % rt === 0) {
+            o++;
+            let row = document.createElement('div');
+            row.className = 'row';
+            row.id = 'r' + o;
+            document.getElementById('container').appendChild(row);
+        }
+
+        let div = document.createElement('div');
+        div.className = 'square';
+        document.getElementById('r' + o).appendChild(div);
+    }
+}
+
+start();
