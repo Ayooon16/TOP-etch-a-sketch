c961897f461d3734b2ade1373303aa913a8b5676
ayooon <michaunapierala@gmail.com>
2024-03-09 17:12:50 +0100

    Complete assigment

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ function start() {
 }
 
 function create(n) {
-    let rt = Math.floor(Math.sqrt(n));
+    document.getElementById("container").innerHTML = ""
+    let rt = n;
     let o = 0;
-
+    n=rt*rt;
     for (let i = 0; i < n; i++) {
         if (i % rt === 0) {
             o++;
@@ -16,9 +17,22 @@ function create(n) {
         }
 
         let div = document.createElement('div');
-        div.className = 'square';
+        div.className = 'square b1';
+        div.onmouseover = hover.bind(div)
         document.getElementById('r' + o).appendChild(div);
     }
 }
 
+function generate(){
+   n=prompt("Width/height of the grid")
+create(n)
+}
+function hover(){
+  //  this.style.backgroundColor='green'
+    a=this.className.split('b')[1]
+    if(a<10){
+        this.className= "square b"+(parseInt(a)+1)
+    }
+  console.log(getComputedStyle(this).getPropertyValue("filter"))
+}
 start();
